a015bb5583bcff53786245c6ffa4b0a7287c08fa
fr2098ed <thetaylor2016@gmail.com>
2022-04-05 13:26:44 +0100

    Fixed Template error

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 import sqlite3
+
 app = Flask(__name__)
 
 accounts = sqlite3.connect("accounts.db")
 videos = sqlite3.connect("videos.db")
-accountsCur =accounts.cursor()
+accountsCur = accounts.cursor()
 videosCur = videos.cursor()
-
 
 
 @app.route("/")
@@ -16,4 +16,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
-
