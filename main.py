f35b53af571d98a6023fd9285bac678c3fc758b7
fr2098ed <thetaylor2016@gmail.com>
2022-04-05 13:19:03 +0100

    Added DBs and main page

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from flask import Flask
-import sqlite3,
+from flask import Flask, render_template, request
+import sqlite3
 app = Flask(__name__)
+
 accounts = sqlite3.connect("accounts.db")
 videos = sqlite3.connect("videos.db")
+accountsCur =accounts.cursor()
+videosCur = videos.cursor()
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
+
